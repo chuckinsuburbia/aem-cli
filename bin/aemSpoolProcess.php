8fa294cd58d1cf49fcb6d97e35c42307e075c7eb
AEM User <aemuser@mv00lp80.(none)>
2014-04-16 10:33:14 -0400

    Added logic to check for valid object in bin/aemSpoolProcess.php, to avoid PHP fatal error

--- bin/aemSpoolProcess.php
+++ bin/aemSpoolProcess.php
@@ -12,45 +12,47 @@ foreach($files as $file) {
 	if($file == "." || $file == ".." || !preg_match("/\.xml$/",$file)) continue;
 	$rmfile = true;
 	$alerts = simplexml_load_file($file);
-	foreach($alerts->children() as $alert) {
-		$tokens = get_object_vars($alert);
-		if($debug) aemlog("Received Alert from ".$tokens['source']);
-		
-		$alertId=createAlert($tokens);
-		if($debug) aemlog("Alert $alertId created");
+	if(is_object($alerts)) {
+		foreach($alerts->children() as $alert) {
+			$tokens = get_object_vars($alert);
+			if($debug) aemlog("Received Alert from ".$tokens['source']);
 
-		#get the path for this source
-		$sPath=getSourcePath($tokens['source']);
+			$alertId=createAlert($tokens);
+			if($debug) aemlog("Alert $alertId created");
 
-		#process each step for this path
-		foreach($sPath as $step){
-			$stepRc=runStep($alertId,$step);
-			if(!$stepRc){
-				aemlog("Source Step Failed, aborting!");
-//				die();
-				$rmfile = false;
-			}
-		}
+			#get the path for this source
+			$sPath=getSourcePath($tokens['source']);
 
-		#check if ticket is blacked out if not set status to open
-		$returnAlert = processAlert($alertId);
-
-		if($returnAlert != false){
-			$destType = $returnAlert['type'];
-			$alertId = $returnAlert['alertId'];
-
-			$dPath=getDestPath($tokens['source'],$destType);
-			print_r($dPath);
-			if($debug) aemlog("running outbound steps");
 			#process each step for this path
-			foreach($dPath as $step){
+			foreach($sPath as $step){
 				$stepRc=runStep($alertId,$step);
 				if(!$stepRc){
-					aemlog("Destination Step Failed!");
+					aemlog("Source Step Failed, aborting!");
+//					die();
+					$rmfile = false;
 				}
 			}
+
+			#check if ticket is blacked out if not set status to open
+			$returnAlert = processAlert($alertId);
+
+			if($returnAlert != false){
+				$destType = $returnAlert['type'];
+				$alertId = $returnAlert['alertId'];
+
+				$dPath=getDestPath($tokens['source'],$destType);
+				print_r($dPath);
+				if($debug) aemlog("running outbound steps");
+				#process each step for this path
+				foreach($dPath as $step){
+					$stepRc=runStep($alertId,$step);
+					if(!$stepRc){
+						aemlog("Destination Step Failed!");
+					}
+				}
+			}
+			if($debug) aemlog("End of Alert Processing for $alertId");
 		}
-		if($debug) aemlog("End of Alert Processing for $alertId");
 	}
 	if($rmfile != false) unlink($file);
 }
