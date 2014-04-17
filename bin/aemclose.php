1c0cfe8bef0fb9c1a2b2cf2a59cfd956a58b8d02
AEM User <aemuser@mv00lp80.(none)>
2014-04-17 11:25:06 -0400

    Fixes to allow AlarmPoint agent to close alerts

--- bin/aemclose.php
+++ bin/aemclose.php
@@ -3,9 +3,9 @@
 # Example command line
 # aemclose.php incident_id=<number>
 $basepath=getenv("AEMBASE");
-require_once($basepath."conf/config.php");
-require_once($basepath."lib/functions.php");
-require_once($basepath.'lib/CronParser.php');
+require_once($basepath."/conf/config.php");
+require_once($basepath."/lib/functions.php");
+require_once($basepath.'/lib/CronParser.php');
 
 $db_tbl_alert="aem_alert";
 
