d6ce63bfcd507385ab8a19406ef00613f39eebae
AEM User <aemuser@mv00lp80.(none)>
2014-05-06 10:16:31 -0400

    Added more debug logging to Tlog script

--- mail/functionsTlog.php
+++ mail/functionsTlog.php
@@ -1,16 +1,20 @@
 <?php
 function tlogProcess($body) {
-	global $emePost;
+	global $emePost,$debug;
 	foreach(explode("\n",$body) as $row) {
+		if($debug) logmsg($row);
 		$s = false;
 		$row = preg_replace("/\s+/","",$row);
 		if($s && strstr($row,",")) {
 			list($store,$time) = explode(",",$row);
 			$url = $emePost."?store=".$store."&timestamp=".$time;
+			if($debug) logmsg($url);
 			$post = file_get_contents($url);
-			logmsg($url);
 		}
-		if(strstr($row,"---START---DATA---")) { $s = true; }
+		if(strstr($row,"---START---DATA---")) { 
+			if($debug) logmsg("Start of data found");
+			$s = true; 
+		}
 	}	
 }
 ?>
