b02f4918fa36c4887a3304c1a9122e390548b5a4
AEM User <aemuser@mv00lp80.(none)>
2014-05-08 14:20:44 -0400

    Fixes to SC monitor script

--- anp/scMonitor/send.sh
+++ anp/scMonitor/send.sh
@@ -23,17 +23,15 @@ if [[ "$CURDATE" != "$FILEDATE" ]]; then
 	fi
 
 	echo "OPEN NEW TICKET" >> $LOG
-	result=$(/in/AEM/servicecenter/anpCreateEntry.php "12345678" "TEST" "TEST" "TEST SC OUTBOUND EMAIL MONITOR" "Warning" "PEMEMAILTEST" "TEST" "TEST" "TEST" 2>&1)
-	echo $result >> $LOG
-	alert=$(echo $result |awk '{print $1}')
-	im=$(echo $result |awk '{print $2}')
-	echo >> $LOG
+	im=$(/in/AEM/servicecenter/anpCreateEntry.php "12345678" "TEST" "TEST" "TEST SC OUTBOUND EMAIL MONITOR" "Warning" "PEMEMAILTEST" "TEST" "TEST" "TEST" 2>&1)
+	success=$?
 	echo $im >$TICKET
-	success=$(echo $result | grep SUCCESS | wc -l)
 	if [[ $success == "0" ]]; then
-		echo $result
+		echo $im
 		exit 1
 	fi
+	echo $im >> $LOG
+	echo >> $LOG
 fi
 
 echo "UPDATE TICKET $IM" >>$LOG
